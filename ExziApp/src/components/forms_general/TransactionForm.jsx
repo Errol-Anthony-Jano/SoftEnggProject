adf7e7edbf28610e5da1a53775a3e7bfb5afa885
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-11-18 13:52:15 +0800

    Transferred subcomponents to separate file for readability

--- ExziApp/src/components/forms_general/TransactionForm.jsx
+++ ExziApp/src/components/forms_general/TransactionForm.jsx
@@ -1,89 +1,51 @@
-import { Field, Input,Listbox, ListboxButton, ListboxOption, ListboxOptions, Textarea } from '@headlessui/react'
+import { Field, Input, Textarea, Switch } from '@headlessui/react'
 import TranscDatePicker from '../form_components/TranscDatePicker'
-import { useState } from 'react'
-import CategorySelector from '../form_components/CategorySelector'
-
-/*
-Fetch the ff. here:
-Income categories of user
-*/
-
-const people = [
-  { id: 1, name: 'Durward Reynolds' },
-  { id: 2, name: 'Kenton Towne' },
-  { id: 3, name: 'Therese Wunsch' },
-  { id: 4, name: 'Benedict Kessler' },
-  { id: 5, name: 'Katelyn Rohan' },
-]
-
-const IncomeSourceField = ({inc_cat_label, exp_src_label, transaction_type}) => {
-    return (
-        <Field as="div" className={`${transaction_type === 'expense' ? 'w-1/3' : 'w-1/2'} h-full flex flex-col`}>
-            <label>{inc_cat_label || exp_src_label}</label>
-            <CategorySelector categoryList={people}/>
-        </Field>
-    )
-}
-
-const ExpenseCategoryField = ({exp_dst_label}) => {
-    return (
-        <Field as="div" className="flex flex-col w-1/3 h-full">
-            <label>{exp_dst_label}</label>
-            <CategorySelector categoryList={people}/>
-        </Field>
-    )
-}
-
-const TransferSourceField = ({transfer_src_label}) => {
-    return (
-        <Field as="div" className="flex flex-col w-1/2">
-            <label>{transfer_src_label}</label>
-            <CategorySelector categoryList={people}/>
-        </Field>
-    )
-}
-
-const TransferDestField = ({transfer_dst_label}) => {
-    return (
-        <Field as="div" className="flex flex-col w-1/2">
-            <label>{transfer_dst_label}</label>
-            <CategorySelector categoryList={people}/>
-        </Field>
-    )
-}
+import {DatePicker, IncomeSourceField, ExpenseCategoryField, TransferSourceField, TransferDestField, BudgetSetter} from './TransactionFormComponents'
 
 const TransactionForm = ({transaction_type, inc_cat_label, exp_src_label, exp_dst_label, transfer_src_label, transfer_dst_label}) => {
+
+    const renderSection = () => {
+        switch (transaction_type) {
+            case 'income':
+                return (
+                    <>
+                        <IncomeSourceField transaction_type={transaction_type} inc_cat_label={inc_cat_label}/>
+                        <DatePicker transaction_type={transaction_type} />
+                    </>
+                )
+            case 'expense':
+                return (
+                    <>
+                        <IncomeSourceField transaction_type={transaction_type} inc_cat_label={inc_cat_label}/>
+                        <ExpenseCategoryField exp_dst_label={exp_dst_label} />
+                        <DatePicker transaction_type={transaction_type} />
+                    </>
+                )
+            case 'transfer':
+                return (
+                    <>
+                        <TransferSourceField transfer_src_label={transfer_src_label} />
+                        <TransferDestField transfer_dst_label={transfer_dst_label} />
+                    </>
+                )
+        }
+    }
+
     return (
         <div className="h-full flex flex-col p-4 gap-4">
-            <div className="flex justify-between w-full gap-4 h-[30%]">
-                {
-                    (transaction_type === 'income' || transaction_type === 'expense') && (
-                        <>
-                            <IncomeSourceField inc_cat_label={inc_cat_label} exp_src_label={exp_src_label} transaction_type={transaction_type}/>
-
-                            { transaction_type === 'expense' && <ExpenseCategoryField exp_dst_label={exp_dst_label}/>}
-
-                            <Field as="div" className={`${transaction_type === 'expense' ? 'w-1/3' : 'w-1/2'} h-full flex flex-col`}>
-                                <label>Select date</label>
-                                <TranscDatePicker />
-                            </Field>
-                        </>
-                    )
-                }
-                {
-                    (transaction_type === "transfer") && (
-                        <div className="flex justify-between w-full gap-4 h-full">
-                            <TransferSourceField transfer_src_label={transfer_src_label} />
-                            <TransferDestField transfer_dst_label={transfer_dst_label} />
-                        </div>
-                    )
-                }
+            <div className="flex justify-between w-full gap-4 shrink-0">
+                {renderSection()}
             </div>
-            <Field as="div" className="flex flex-col h-[20%]">
+            {
+                <div className="shrink-0">
+                    {transaction_type === 'expense' && <BudgetSetter budget_set_label="Set budget?" />}
+                </div>
+            }
+            <Field as="div" className="flex flex-col shrink-0">
                 <label>Enter amount</label>
                 <Input className="border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow"/>
             </Field>
-            <Field as="div" className="flex flex-col h-[50%]">
+            <Field as="div" className="flex flex-col shrink-0 grow">
                 <label>Enter note</label>
                 <Textarea className="border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow"/>
             </Field>
