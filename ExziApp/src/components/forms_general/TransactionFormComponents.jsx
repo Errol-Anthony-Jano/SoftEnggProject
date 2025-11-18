7b7da9c30f51641f9341c0e93f48a21ca3d30aa7
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-11-18 13:52:45 +0800

    Created file for subcomponents of TransactionForm

---
+++ ExziApp/src/components/forms_general/TransactionFormComponents.jsx
@@ -0,0 +1,74 @@
+import { Field, Input, Textarea, Switch } from '@headlessui/react'
+import TranscDatePicker from '../form_components/TranscDatePicker'
+import { useState } from 'react'
+import CategorySelector from '../form_components/CategorySelector'
+const people = [
+  { id: 1, name: 'Durward Reynolds' },
+  { id: 2, name: 'Kenton Towne' },
+  { id: 3, name: 'Therese Wunsch' },
+  { id: 4, name: 'Benedict Kessler' },
+  { id: 5, name: 'Katelyn Rohan' },
+]
+
+export const IncomeSourceField = ({inc_cat_label, exp_src_label, transaction_type}) => {
+    return (
+        <Field as="div" className={`${transaction_type === 'expense' ? 'w-1/3' : 'w-1/2'} h-full flex flex-col`}>
+            <label>{inc_cat_label || exp_src_label}</label>
+            <CategorySelector categoryList={people}/>
+        </Field>
+    )
+}
+
+export const ExpenseCategoryField = ({exp_dst_label}) => {
+    return (
+        <Field as="div" className="flex flex-col w-1/3 h-full">
+            <label>{exp_dst_label}</label>
+            <CategorySelector categoryList={people}/>
+        </Field>
+    )
+}
+
+export const TransferSourceField = ({transfer_src_label}) => {
+    return (
+        <Field as="div" className="flex flex-col w-1/2">
+            <label>{transfer_src_label}</label>
+            <CategorySelector categoryList={people}/>
+        </Field>
+    )
+}
+
+export const TransferDestField = ({transfer_dst_label}) => {
+    return (
+        <Field as="div" className="flex flex-col w-1/2">
+            <label>{transfer_dst_label}</label>
+            <CategorySelector categoryList={people}/>
+        </Field>
+    )
+}
+
+export const BudgetSetter = ({budget_set_label}) => {
+    const [enabled, setEnabled] = useState(false)
+
+    return (
+        <div className="flex justify-between items-center h-full w-full">
+            <label>{budget_set_label}</label>
+            <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-checked:bg-blue-600"
+            >
+                <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-6" />
+            </Switch>
+            <Input className="w-1/2 border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out"></Input>
+        </div>
+    )
+}
+
+export const DatePicker = ({transaction_type}) => {
+    return (
+        <Field as="div" className={`${transaction_type === 'expense' ? 'w-1/3' : 'w-1/2'} h-full flex flex-col`}>
+            <label>Select date</label>
+            <TranscDatePicker />
+        </Field>
+    )
+}
