d56d50c48e5eef810e3ee5722f53e3bd3c9e6016
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-11-19 16:02:40 +0800

    Added dialog provider to content outlet

--- ExziApp/src/components/top_level_components/PageContent.jsx
+++ ExziApp/src/components/top_level_components/PageContent.jsx
@@ -1,8 +1,12 @@
 //page content switcher
 import Header from './Header.jsx'
-import BaseModal from '../modal_components/BaseModal.jsx'
+import BaseForm from "../forms_general/BaseForm.jsx"
+import BaseDialog from '../dialogs/BaseDialog.jsx'
 import { Outlet , useMatches } from 'react-router'
-import { useState } from 'react'
+import { useState, createContext, useMemo } from 'react'
+import { ModalContext } from '../../contexts/ModalContext.js'
+import { DialogContext } from '../../contexts/DialogContext.js'
+
 const PageContent = () => {
     const matches = useMatches();
     const { handle } = matches[matches.length - 1] || {}
@@ -11,28 +15,41 @@ const PageContent = () => {
     const [headerButton, setHeaderButton] = useState(null)
     const [modalType, setModalType] = useState(null) // null -> no modal is to be shown
     const [modalHeader, setModalHeader] = useState(null)
-    // setModalType -> opens modal depending on 
-    
+    const [dialogType, setDialogType] = useState(null)
+
+    const dialogContext = useMemo(() => ({
+        setDialogType,
+    }), [setDialogType])
+
+    const modalContext = useMemo(() => ({
+        setModalType,
+        setModalHeader
+    }), [setModalType, setModalHeader])
+
     const closeModal = () => {
         setModalType(null)
     }
+
     return (
-        <div className="h-full grid grid-rows-12 grid-cols-12 w-full">
-            <Header className="row-start-[1] row-end-[3] col-start-[1] col-end-[13]" main_msg={main_msg} sub_msg={sub_msg} button={headerButton}></Header>
-            
-            <div className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]">
-                <Outlet context={{setHeaderButton, setModalType, setModalHeader}} className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]"/>
-            </div>
-            {
-                
+        <DialogContext.Provider value={dialogContext}>
+            <ModalContext.Provider value={modalContext}>
+            <div className="h-full grid grid-rows-12 grid-cols-12 w-full">
+                <Header className="row-start-[1] row-end-[3] col-start-[1] col-end-[13]" main_msg={main_msg} sub_msg={sub_msg} button={headerButton}></Header>
+                <div className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]">
+                    <Outlet context={{setHeaderButton, setModalType, setModalHeader}} className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]"/>
+                </div>
+                {
                     modalType && (
-                        <BaseModal onClose={closeModal} display={modalType} displayName={modalHeader}>
+                        <BaseForm onClose={closeModal} display={modalType} displayName={modalHeader}>
                             {modalType}
-                        </BaseModal>
+                        </BaseForm>
                     )
-                
-            }
-        </div>
+                }
+                {dialogType}
+            </div>
+            </ModalContext.Provider>
+        </DialogContext.Provider>
+        
     )
 }
 
