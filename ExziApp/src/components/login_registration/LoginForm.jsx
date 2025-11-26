7af2d5dc397a725620bc95609cc230db104f1a22
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-11-26 22:45:03 +0800

    Added useNavigate hook for login logic

--- ExziApp/src/components/login_registration/LoginForm.jsx
+++ ExziApp/src/components/login_registration/LoginForm.jsx
@@ -1,6 +1,19 @@
 import { Field, Input } from "@headlessui/react"
+import { useNavigate } from "react-router";
 
 const LoginForm = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        // 3. IMPORTANT: Prevent the browser from refreshing the page
+        e.preventDefault();
+
+        // (In the future, your DB check logic goes here)
+
+        // 4. Navigate to the dashboard (root path)
+        navigate('/');
+    };
     return (
         <form className="flex flex-col h-full p-4 gap-4 justify-between">
             <div className="flex flex-col w-full h-[90%] gap-4">
@@ -16,7 +29,7 @@ const LoginForm = () => {
                     <Input type="password" className="border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow" />
                 </Field>
             </div>
-            <button className="w-full bg-[#adc9d4] h-[10%] rounded-lg">Register</button>
+            <button className="w-full bg-[#adc9d4] h-[10%] rounded-lg" onClick={handleLogin}>Login</button>
         </form>
     )
 }
