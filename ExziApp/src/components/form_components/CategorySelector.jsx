64d68a51e82ba61a3ede38525e0c8713fef3beb4
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-12-02 13:52:17 +0800

    Minor changes

--- ExziApp/src/components/form_components/CategorySelector.jsx
+++ ExziApp/src/components/form_components/CategorySelector.jsx
@@ -1,17 +1,18 @@
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
 import { useState } from 'react'
 
-const CategorySelector = ({ categoryList }) => {
-    const [currCategory, setCurrCategory] = useState(categoryList[0])
+const CategorySelector = ({ categoryList, input_state, input_setter}) => {
+
+    const buttonText = input_state?.name || "Click to select category";
 
     return (
-        <Listbox as="div" className="w-full h-full" value={currCategory} onChange={setCurrCategory}>
-            <ListboxButton className="w-full h-full border border-[#646464] rounded-sm data-focus:border-white hover:border hover:border-white hover:transition duration-300 ease-in-out">{currCategory.name}</ListboxButton>
+        <Listbox as="div" className="w-full h-full" value={input_state} onChange={input_setter}>
+            <ListboxButton className="w-full h-full border border-[#646464] rounded-sm data-focus:border-white hover:border hover:border-white hover:transition duration-300 ease-in-out">{buttonText}</ListboxButton>
             <ListboxOptions anchor="bottom" className="z-[1001] w-(--button-width) border border-[#646464] rounded-sm">
                 {categoryList.map((person) => (
-                <ListboxOption key={person.id} value={person} className="bg-black data-focus:bg-gray-900">
-                    {person.name}
-                </ListboxOption>
+                    <ListboxOption key={person.id} value={person} className="bg-black data-focus:bg-gray-900">
+                        {person.name}
+                    </ListboxOption>
                 ))}
             </ListboxOptions>
         </Listbox>
