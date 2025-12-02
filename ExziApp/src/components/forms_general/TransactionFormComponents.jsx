5977b203d8281e291a49d81899a990e1b032b70d
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-12-02 14:06:17 +0800

    Added states and state updates

--- ExziApp/src/components/forms_general/TransactionFormComponents.jsx
+++ ExziApp/src/components/forms_general/TransactionFormComponents.jsx
@@ -2,6 +2,7 @@ import { Field, Input, Textarea, Switch } from '@headlessui/react'
 import TranscDatePicker from '../form_components/TranscDatePicker'
 import { useState } from 'react'
 import CategorySelector from '../form_components/CategorySelector'
+
 const people = [
   { id: 1, name: 'Durward Reynolds' },
   { id: 2, name: 'Kenton Towne' },
@@ -10,47 +11,47 @@ const people = [
   { id: 5, name: 'Katelyn Rohan' },
 ]
 
-export const IncomeSourceField = ({inc_cat_label, exp_src_label, transaction_type}) => {
+export const IncomeSourceField = ({inc_cat_label, exp_src_label, transaction_type, input_state, input_setter}) => {
     return (
         <Field as="div" className={`${transaction_type === 'expense' ? 'w-1/3' : 'w-1/2'} h-full flex flex-col`}>
             <label>{inc_cat_label || exp_src_label}</label>
-            <CategorySelector categoryList={people}/>
+            <CategorySelector input_state={input_state} input_setter={input_setter} categoryList={people}/>
         </Field>
     )
 }
 
-export const ExpenseCategoryField = ({exp_dst_label}) => {
+export const ExpenseCategoryField = ({exp_dst_label, input_state, input_setter}) => {
     return (
         <Field as="div" className="flex flex-col w-1/3 h-full">
             <label>{exp_dst_label}</label>
-            <CategorySelector categoryList={people}/>
+            <CategorySelector input_state={input_state} input_setter={input_setter} categoryList={people}/>
         </Field>
     )
 }
 
-export const TransferSourceField = ({transfer_src_label}) => {
+export const TransferSourceField = ({transfer_src_label, input_state, input_setter}) => {
     return (
         <Field as="div" className="flex flex-col w-1/2">
             <label>{transfer_src_label}</label>
-            <CategorySelector categoryList={people}/>
+            <CategorySelector input_state={input_state} input_setter={input_setter} categoryList={people}/>
         </Field>
     )
 }
 
-export const TransferDestField = ({transfer_dst_label}) => {
+export const TransferDestField = ({transfer_dst_label, input_state, input_setter}) => {
     return (
         <Field as="div" className="flex flex-col w-1/2">
             <label>{transfer_dst_label}</label>
-            <CategorySelector categoryList={people}/>
+            <CategorySelector input_state={input_state} input_setter={input_setter} categoryList={people} />
         </Field>
     )
 }
 
-export const DatePicker = ({transaction_type}) => {
+export const DatePicker = ({transaction_type, input_state, input_setter}) => {
     return (
         <Field as="div" className={`${transaction_type === 'expense' ? 'w-1/3' : 'w-1/2'} h-full flex flex-col`}>
             <label>Select date</label>
-            <TranscDatePicker />
+            <TranscDatePicker input_state={input_state} input_setter={input_setter} />
         </Field>
     )
 }
