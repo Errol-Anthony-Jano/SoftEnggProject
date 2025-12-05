962abe614deb32e4d1d56248a2b74175db2fb74a
Errol Anthony Jano <154906137+Errol-Anthony-Jano@users.noreply.github.com>
2025-12-05 15:52:59 +0800

    Simplified structure of PageContent

--- ExziApp/src/components/top_level_components/PageContent.jsx
+++ ExziApp/src/components/top_level_components/PageContent.jsx
@@ -6,6 +6,12 @@ import { Outlet , useMatches } from 'react-router'
 import { useState, createContext, useMemo } from 'react'
 import { ModalContext } from '../../contexts/ModalContext.js'
 import { DialogContext } from '../../contexts/DialogContext.js'
+import { useAtomValue, useSetAtom } from "jotai"
+import {
+    formContentAtom,
+    formHeaderAtom,
+    dialogContentAtom,
+} from "../forms_general/FormAtoms.js"
 
 const PageContent = () => {
     const matches = useMatches();
@@ -13,35 +19,18 @@ const PageContent = () => {
     const { main_msg, sub_msg } = handle || {};
 
     const [headerButton, setHeaderButton] = useState(null)
-    const [formType, setFormType] = useState(null) // null -> no modal is to be shown
-    const [formHeader, setFormHeader] = useState(null)
-    const [dialogType, setDialogType] = useState(null)
 
-    const dialogContext = useMemo(() => ({
-        setDialogType,
-    }), [setDialogType])
-
-    const formContext = useMemo(() => ({
-        setFormType,
-        setFormHeader
-    }), [setFormType, setFormHeader])
-
-    const closeForm = () => {
-        setModalType(null)
-    }
+    const outletContextValue = useMemo(() => ({
+        setHeaderButton,
+    }), [setHeaderButton])
 
     return (
-        <DialogContext.Provider value={dialogContext}>
-            <ModalContext.Provider value={formContext}>
-            <div className="h-full grid grid-rows-12 grid-cols-12 w-full">
-                <Header className="row-start-[1] row-end-[3] col-start-[1] col-end-[13]" main_msg={main_msg} sub_msg={sub_msg} button={headerButton}></Header>
-                <div className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]">
-                    <Outlet context={{headerButton, setHeaderButton, formType, setFormType, formHeader, setFormHeader, dialogType, setDialogType}} className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]"/>
-                </div>
+        <div className="h-full grid grid-rows-12 grid-cols-12 w-full">
+            <Header className="row-start-[1] row-end-[3] col-start-[1] col-end-[13]" main_msg={main_msg} sub_msg={sub_msg} button={headerButton}></Header>
+            <div className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]">
+                <Outlet context={outletContextValue} className="row-start-[3] row-end-[13] col-start-[1] col-end-[13]"/>
             </div>
-            </ModalContext.Provider>
-        </DialogContext.Provider>
-        
+        </div>
     )
 }
 
